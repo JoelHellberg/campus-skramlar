ab124497de67505a19c89fbbfb05b7e648e93509
Joel Hellberg <joehe737@student.liu.se>
2025-07-08 16:22:42 +0200

    Bytt till SVG logga

--- sections/hello.tsx
+++ sections/hello.tsx
@@ -5,9 +5,9 @@ export default function Hello() {
       <div className="relative w-full">
         {/* Logo & Text */}
         <div className="flex flex-col items-center justify-center absolute inset-0 z-20">
-          <img src="/logo.png" alt="logo" className="w-80" />
+          <img src="/logo.svg" alt="logo" className="w-82" />
           <div
-            className="-mt-14 text-[#ACCAB2] flex flex-col items-center"
+            className="-mt-15 text-[#ACCAB2] flex flex-col items-center"
             style={{
               textShadow:
                 "-5px 5px 0 white, -12px 12px 0 #D06224, -18px 18px 0 #8A8635",
@@ -20,7 +20,7 @@ export default function Hello() {
 
         {/* Logo shadow */}
         <div className="flex items-center justify-center absolute inset-0 z-15">
-          <div className="h-82 w-82 -mt-28 rounded-full bg-black opacity-20" />
+          <div className="h-82 w-82 -mt-26 rounded-full bg-black opacity-20" />
         </div>
 
         {/* Beige Overlay to dimm gif intensity */}

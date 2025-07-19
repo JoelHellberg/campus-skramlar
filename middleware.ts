70fefc5185339f5c4048b10714e77614152ab0f5
Joel Hellberg <joehe737@student.liu.se>
2025-07-19 14:44:13 +0200

    Implementerat middleware för redirection

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifySession } from "./app/_lib/authentication";
+
+export async function middleware(request: NextRequest) {
+  const currentPath = request.nextUrl.pathname;
+
+  const protectedRoutes = [
+    "/forening/profil",
+    "/forening/insamlat",
+    "/forening/uppdateringar",
+  ];
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    currentPath.startsWith(route)
+  );
+
+  if (isProtectedRoute) {
+    const session = await verifySession();
+
+    if (!session) {
+      return NextResponse.redirect(new URL("/forening/login", request.nextUrl));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/forening/profil",
+    "/forening/insamlat",
+    "/forening/uppdateringar",
+  ],
+};
